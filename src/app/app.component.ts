1743b7fd1e4cdebc8ce25d3bb765f3c2be828f4f
Anuz <ceanuz66@gmail.com>
2023-01-25 19:58:26 +0600

    nested formGroup

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup
+
+  ngOnInit() {
+    // form control is key value pair
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup( {
+        'username': new FormControl(null, Validators.required), // validator reference
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male')
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+    
+  }
+}
