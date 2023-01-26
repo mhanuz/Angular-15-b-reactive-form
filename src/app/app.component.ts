a7a1cd1105fb7e765fd14b03339933a0d75e1e06
Anuz <ceanuz66@gmail.com>
2023-01-26 13:48:31 +0600

    formArrya,setvalue,pathvalue,error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +9,59 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup
+  forbiddenUserName = ["Md.","Chris","Anna"]
 
   ngOnInit() {
     // form control is key value pair
     this.signupForm = new FormGroup({
       'userData': new FormGroup( {
-        'username': new FormControl(null, Validators.required), // validator reference
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), // validator reference
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'gender': new FormControl('male')
-    })
+      'gender': new FormControl('male'),
+      'hobbies' : new FormArray([]) // form array hold an array of controls
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // )
+
+    this.signupForm.statusChanges.subscribe(
+      (status)=> console.log(status)
+      
+    )
+     this.signupForm.setValue({
+      'userData':{
+        'username': 'Max',
+        'email': "max@test.com"
+      },
+      'gender': 'female',
+      'hobbies': []
+     })
+
+     this.signupForm.patchValue({
+      'userData':{
+        'username': 'Anna',
+      }
+     })
   }
 
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset()
     
   }
+
+  onAddHobby(){
+    // <FormArray>:casting, this bracket enclosed part tells the typescript this is my array
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+  // validator is a function that angular execute automatically
+  forbiddenNames (control: FormControl): {[s: string]: boolean}{
+    if (this.forbiddenUserName.indexOf(control.value)!==-1){
+      return {'nameIsForbidden': true};
+    }
+    return null; // if the validation is succesfull retun nothing or null
+  }
+
 }
